945af37147fc7d21340839a505a1677987da2849
[Caira] <[cmajor7@student.gsu.edu]>
2026-04-08 02:28:57 -0400

    Fixed formatting for logout

--- logout.php
+++ logout.php
@@ -3,7 +3,7 @@
 require_once 'config.php';
 require_once 'functions.php';
 
-// Only clear login state and game this keep users and leaderboard intact
+// keeps users and leaderboard intact
 unset($_SESSION['user']);
 unset($_SESSION['game']);
 ?>
